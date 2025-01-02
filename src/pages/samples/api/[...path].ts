24c1e04b3683049d5a08826916f2f2c4d47e1c7d
Alex Astrum <alexastrum@gmail.com>
2025-01-02 13:07:10 -0500

    Add Hono Swagger UI and Zod OpenAPI integration; update API route

--- src/pages/samples/api/[...path].ts
+++ src/pages/samples/api/[...path].ts
@@ -1,13 +1,80 @@
-import type { APIRoute } from "astro";
+import { z } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { swaggerUI } from "@hono/swagger-ui";
+import type { APIContext, APIRoute } from "astro";
 
 export const prerender = false;
 
-export const GET: APIRoute = ({ params, request }) => {
-  return new Response(JSON.stringify({ path: params.path }));
-};
+const PATH_PREFIX = "/samples/api";
 
-export const POST: APIRoute = async ({ params, request }) => {
-  return new Response(
-    JSON.stringify({ data: await request.json(), path: params.path })
-  );
-};
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: "id",
+        in: "path",
+      },
+      example: "1212121",
+    }),
+});
+
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: "123",
+    }),
+    name: z.string().openapi({
+      example: "John Doe",
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi("User");
+
+const route = createRoute({
+  method: "post",
+  path: `${PATH_PREFIX}/users/{id}`,
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Retrieve the user",
+    },
+  },
+});
+
+// Map Astro's current APIContext to Hono's `context.env`
+const app = new OpenAPIHono<{ Bindings: APIContext }>();
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 20,
+    name: "Ultra-man",
+  });
+});
+
+// The OpenAPI documentation will be available at /doc
+app.doc(`${PATH_PREFIX}/doc`, {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+  },
+});
+
+app.get(`${PATH_PREFIX}`, swaggerUI({ url: `${PATH_PREFIX}/doc` }));
+
+export const GET: APIRoute = (c) => app.fetch(c.request, c);
+export const POST: APIRoute = (c) => app.fetch(c.request, c);
